843a68775367f6408458e71b8ce3ece6e2d79ca3
EmilyXinyi <xinyi_0302@yahoo.com>
2021-07-01 15:02:53 +0000

    every up until containerisation

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask, request, render_template
 from dotenv import load_dotenv
 from . import db
-# from werkzeug.security import check_password_hash, generate_password_hash
-# from app.db import get_db
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.db import get_db
 
 
 load_dotenv()
@@ -20,56 +20,56 @@ def em():
 def health(): 
     return 'health works'
 
-# @app.route('/register', method=('GET', 'POST'))
-# def register():
-#     if request.method == 'POST':
-#         username = request.form.get('username')
-#         password = request.form.get('password')
-#         db = get_db()
-#         error = None
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
 
-#         if not username:
-#             error = 'Username is required.'
-#         elif not password:
-#             error = 'Password is required.'
-#         elif db.execute(
-#             'SELECT id FROM user WHERE username = ?', (username,)
-#         ).fetchone() is not None:
-#             error = f"User {username} is already registered."
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
 
-#         if error is None:
-#             db.execute(
-#                 'INSERT INTO user (username, password) VALUES (?, ?)',
-#                 (username, generate_password_hash(password))
-#             )
-#             db.commit()
-#             return f"User {username} created successfully"
-#         else:
-#             return error, 418
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
 
-#     ## TODO: Return a restister page
-#     return "Register Page not yet implemented", 501
+    ## TODO: Return a restister page
+    return "Register Page not yet implemented", 501
 
-# @app.route('/login', methods=('GET', 'POST'))
-# def login():
-#     if request.method == 'POST':
-#         username = request.form.get('username')
-#         password = request.form.get('password')
-#         db = get_db()
-#         error = None
-#         user = db.execute(
-#             'SELECT * FROM user WHERE username = ?', (username,)
-#         ).fetchone()
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
-#         if user is None:
-#             error = 'Incorrect username.'
-#         elif not check_password_hash(user['password'], password):
-#             error = 'Incorrect password.'
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
 
-#         if error is None:
-#             return "Login Successful", 200 
-#         else:
-#             return error, 418
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
     
-#     ## TODO: Return a login page
-#     return "Login Page not yet implemented", 501
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501

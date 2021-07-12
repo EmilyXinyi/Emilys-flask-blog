970f69324965b3e642404444ed032e366a2ea741
EmilyXinyi <xinyi_0302@yahoo.com>
2021-07-12 11:36:33 -0400

    frist attempt: deployment test

---
+++ prod-test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+curl -s -o /dev/null -w "%{http_code}" https://emilyxinyi-chen.duckdns.org
+res=$?
+if test "$res" != "200"
+then
+    echo "home page failed with: $res"
+else
+    echo "home page success"
+fi
+
+curl -s -o /dev/null -w "%{http_code}" https://emilyxinyi-chen.duckdns.org/health
+res=$?
+if test "$res" != "200"
+then
+    echo "health page failed with: $res"
+else
+    echo "health page success"
+fi
+
+curl -s -o /dev/null -w "%{http_code}" https://emilyxinyi-chen.duckdns.org/register
+res=$?
+if test "$res" != "200"
+then
+    echo "register page failed with: $res"
+else
+    echo "register page success"
+fi
+
+curl -s -o /dev/null -w "%{http_code}" https://emilyxinyi-chen.duckdns.org/login
+res=$?
+if test "$res" != "200"
+then
+    echo "login page failed with: $res"
+else
+    echo "login page success"
+fi
